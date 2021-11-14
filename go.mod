ff35a0b2c055886e7760089adbdf83f5b638c67c
ItisCaleb <40360126+ItisCaleb@users.noreply.github.com>
2021-11-14 16:49:42 +0800

    add limiter

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module 111college
 
 go 1.16
 
-require github.com/bitly/go-simplejson v0.5.0 // indirect
+require (
+	github.com/bitly/go-simplejson v0.5.0 // indirect
+	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
+)
--- routes/routes.go
+++ routes/routes.go
@@ -3,9 +3,14 @@ package routes
 import (
 	"fmt"
 	"github.com/bitly/go-simplejson"
+	"golang.org/x/time/rate"
 	"io/ioutil"
+	"net"
 	"net/http"
+	"time"
 )
+
+var Limiters = map[string]*rate.Limiter{}
 
 func Root(rw http.ResponseWriter, r *http.Request) {
 	http.ServeFile(rw, r, "./index.html")
@@ -18,6 +23,16 @@ func Data(rw http.ResponseWriter, r *http.Request) {
 func SearchSchool(rw http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	if r.Method == "POST" {
+		ip, _, _:= net.SplitHostPort(r.RemoteAddr)
+		if limiter,ok := Limiters[ip];ok {
+			if allow := limiter.AllowN(time.Now(),5);!allow{
+				rw.WriteHeader(http.StatusTooManyRequests)
+				fmt.Fprintf(rw,"星爆爆")
+				return
+			}
+		}else {
+			Limiters[ip] = rate.NewLimiter(1,150)
+		}
 		data,_ := ioutil.ReadAll(r.Body)
 		body,err := simplejson.NewJson(data)
 		if err != nil {
